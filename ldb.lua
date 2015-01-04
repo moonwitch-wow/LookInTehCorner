e3f39538a3748391e339ad5648d68d3297b3df6d
moonwitch <kelly.crabbe@gmail.com>
2015-01-04 16:33:34 +0100

    added a little LDB - this is more for DPS tracking since I don't use a DPS meter anymore.

---
+++ ldb.lua
@@ -0,0 +1,15 @@
+------------------------------------------------------------------------
+-- Simple DataBroker frame at the bottom of the minimap
+------------------------------------------------------------------------
+local brokerwidth = Minimap:GetWidth()
+
+local LCS = LibStub("LibCargoShip-2.1")
+local block = LCS:CreateBlock{
+    width = brokerwidth,
+    parent = Minimap,
+    noIcon = false,
+    scale = 1,
+    fontSize = 11
+}
+block:SetDataObject("DPS")
+block:SetPoint("TOP", Minimap, "BOTTOM", 0, 0)
